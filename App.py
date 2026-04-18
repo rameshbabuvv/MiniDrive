38c97312e17a95a55f90fb513d46121d61ab32e2
Ramesh Babu <rameshbabuvv99@gmail.com>
2026-04-18 17:45:46 +0530

    Ramesh: Added upload API

--- App.py
+++ App.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, request
+import os
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route("/")
 def home():
     return "MiniDrive Running"
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    file = request.files['file']
+    filepath = os.path.join(UPLOAD_FOLDER, file.filename)
+    file.save(filepath)
+    return f"Uploaded {file.filename}"
 
 if __name__ == "__main__":
     app.run(debug=True)
